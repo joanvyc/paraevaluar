4b1f5135af64fbd14eb1f507c4506fb7b1a1e955
Joan Vinyals Ylla-Català <joanvyc@ieee.org>
2019-03-17 19:40:43 +0100

    Project rename.

--- create_deb.sh
+++ create_deb.sh
@@ -1,3 +1,4 @@
-cp evaluate evaluate_1.0-1/usr/local/bin/
+mkdir -p paraevaluar_1.0-1/usr/local/bin
+cp paraevaluar paraevaluar_1.0-1/usr/local/bin/
 
-dpkg-deb --build evaluate_1.0-1
+dpkg-deb --build paraevaluar_1.0-1
