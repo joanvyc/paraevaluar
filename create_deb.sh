d856685ed4e38591d8b9290cefc9300fc82830cf
Guillem Ramirez Miranda <19.guillemramirez19@gmail.com>
2019-04-03 12:07:43 +0200

    Paraevaluar 2.1-0 release

--- create_deb.sh
+++ create_deb.sh
@@ -1,4 +1,4 @@
-mkdir -p paraevaluar_2.0-1/usr/local/bin
-cp paraevaluar paraevaluar_2.0-1/usr/local/bin/
+mkdir -p paraevaluar_2.1-0/usr/local/bin
+cp paraevaluar paraevaluar_2.1-0/usr/local/bin/
 
-dpkg-deb --build paraevaluar_2.0-1
+dpkg-deb --build paraevaluar_2.1-0
